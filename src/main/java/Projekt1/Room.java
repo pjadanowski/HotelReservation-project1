7383bbe90a2b004a067edeae685a54cf7c045037
Paweł Jadanowski <pjadanowski@sigma.ug.edu.pl>
2018-04-05 21:58:31 +0200

    added Room class

---
+++ src/main/java/Projekt1/Room.java
@@ -0,0 +1,86 @@
+package Projekt1;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class Room {
+
+    private static  String FILE = "src/main/resources/rooms.json";
+    private int roomNumber;
+    private boolean avaliable;
+
+
+    public Room(int roomNumber , boolean avaliable) {
+        this.roomNumber = roomNumber;
+        this.avaliable = avaliable;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public void setAvaliable(boolean avaliable) {
+        this.avaliable = avaliable;
+    }
+
+    public boolean isAvaliable(){
+        return avaliable;
+    }
+
+    public String readFile(String fileName){
+
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines( Paths.get(fileName), StandardCharsets.UTF_8))
+        {
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return contentBuilder.toString();
+    }
+
+    public void markRoomAsUnavaliable(){
+//        Room[] rooms = new Gson().fromJson(this.readFile(FILE), Room[].class);
+        JsonObject fromFileObject = new Gson().fromJson(readFile(FILE), JsonObject.class);
+        JsonArray mainObject = fromFileObject.getAsJsonArray("rooms");
+
+        for (JsonElement room: mainObject) {
+            if (room.isJsonObject())
+            {
+                JsonObject r = room.getAsJsonObject();
+                if (Integer.parseInt(r.get("room").getAsString()) == this.getRoomNumber()){
+                    r.remove("avaliable");
+                    r.addProperty("avaliable", false);
+                }
+            }
+        }
+        try {
+            FileWriter fileWriter = new FileWriter(new File(FILE));
+            fileWriter.write(fromFileObject.toString());
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        System.out.println(fromFileObject.toString());
+    }
+
+
+}
