52489655a316dd63b4ff6115907947ff928a45b2
Paweł Jadanowski <pjadanowski@sigma.ug.edu.pl>
2018-04-06 13:44:31 +0200

    marking room as unavaliable after booking

--- src/main/java/Projekt1/Reservation.java
+++ src/main/java/Projekt1/Reservation.java
@@ -56,6 +56,8 @@ public class Reservation {
         // save it to file
         updateFile(fromFileObject);
 
+        // set reserved room as unavaliable
+        room.setAvaliable(false);
     }
 
     public String readFile(String fileName){
